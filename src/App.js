7acc4f1167ba95913b3b337892544447261ddbce
Kasech Isaac <kasechisaac@Kasechs-MacBook-Air.local>
2020-08-26 17:09:52 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import {connect} from "react-redux";
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
-const App = () => {
+const App = (props) => {
   const state = {
     additionalPrice: 0,
     car: {
@@ -22,19 +22,25 @@ const App = () => {
       { id: 4, name: 'Rear spoiler', price: 250 }
     ]
   };
-
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header car={props.car} />
+        <AddedFeatures car={props.car} />
       </div>
       <div className="box">
         <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <Total car={props.car} additionalPrice={props.additionalPrice} />
       </div>
     </div>
   );
 };
 
-export default App;
+const mapStateToProps=state=>{
+  return{
+    additionalPrice:state.additionalPrice,
+    car:state.car,
+    additionalFeatures:state.additionalFeatures
+  };
+};
+connect(mapStateToProps,{}) (App);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import {creatStore} from"redux"
+import{reducer} from"../public/reducers/carReducer"
+import {Provider} from "react-redux"
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = creatStore(reducer)
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store ={store}><App /> </Provider>, rootElement);
+
+
